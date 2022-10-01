910bc7ca929c31094b5feaa833164c4c2cfcc316
rizkypsr <rizkypsrr@gmail.com>
2022-10-01 08:34:08 +0700

    create isVowel function

---
+++ is_vowel.dart
@@ -0,0 +1,13 @@
+/// Checks if [char] is a Vowel.
+bool isVowel(String char) {
+  return char == 'a' ||
+      char == 'e' ||
+      char == 'i' ||
+      char == 'o' ||
+      char == 'u' ||
+      char == 'A' ||
+      char == 'E' ||
+      char == 'I' ||
+      char == 'O' ||
+      char == 'U';
+}
