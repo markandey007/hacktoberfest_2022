39c0c5f1b70ed033609cb1a38f6a2f36d95d86c3
Computer-Master <70250359+rahul-d0gney@users.noreply.github.com>
2022-10-12 07:42:56 +0530

    Python Palindrome Number
    I have done issue.

---
+++ Palindrome_Python.py
@@ -0,0 +1,15 @@
+## Palindrome Python Program 
+
+string=input(("Enter a string:"))
+if(string==string[::-1]):
+     print("The string is a palindrome")
+else:
+     print("Not a palindrome")
+    
+''' 
+Input:-Enter a string: madam
+Output:The string is a palindrome
+
+
+
+
