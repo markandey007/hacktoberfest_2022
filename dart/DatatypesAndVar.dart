d6050249d34faeafc91f07e180622026b17a3b5a
Neha Rathi <39767135+neharathig@users.noreply.github.com>
2022-10-14 21:48:29 +0530

    Create DatatypesAndVar.dart

---
+++ dart/DatatypesAndVar.dart
@@ -0,0 +1,28 @@
+void main()
+{
+  //num : int
+  int score =20;
+  var count=108;      // It is inferred as integer automaticly by the compiler
+  int hexvalue = 0xADCBAE41;
+
+  //num : double
+  double percentage=93.4;
+  double exponent = 1.43e3;
+
+  
+  //string
+  String name = "Krishna";
+  String def ;
+  //Boolean
+  bool present =true;
+
+  // all data types in dart are object. Therefore there initail val is null.
+
+  print(score);
+  print(percentage);
+  print(count);
+  print(hexvalue);
+  print(exponent);
+  print (def);   //null
+
+}
